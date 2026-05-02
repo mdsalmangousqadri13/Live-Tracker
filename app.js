8b832484d950762e9e892a0c9b2818a01a59b925
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2026-05-02 22:48:12 +0530

    git ignore and vercel

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get("/", function (req, res) {
 });
 
 // Start server
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
